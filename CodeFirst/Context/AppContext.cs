81cd29539a5753032e4a5b282c68903c50308538
brock <anup.achwal@greytrixindia.com>
2018-01-05 12:32:02 +0530

    Project Uploaded

---
+++ CodeFirst/Context/AppContext.cs
@@ -0,0 +1,20 @@
+﻿using CodeFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirst.Context
+{
+    public class AppContext : DbContext
+    {
+        public AppContext()
+            : base("appConnection")
+        {
+        }
+
+        public IDbSet<Department> Departments { get; set; }
+        public IDbSet<Employee> Employees { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# mvc5codefirstapproach
+MVC 5 Code First Approach
+
+This is simple demo with minimum Code for Code First Approach in MVC 5.
